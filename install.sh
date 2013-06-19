b5fb38192fab9bd437577f68b2bd0a9f03ca358a
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2013-06-20 00:52:16 +0200

    install.sh does not longer delete with rsync

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 
-rsync --exclude ".git" --exclude ".gitmodules" --exclude "install.sh" -av --delete . ~
+rsync --exclude ".git" --exclude ".gitmodules" --exclude "install.sh" -av . ~
