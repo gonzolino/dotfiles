5fdd6d90717c03c945c4ea9f09c7cf8c3f9a182c
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2013-12-20 13:13:57 +0100

    Don`t copy README.md for isntallation

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 
-rsync --exclude ".git" --exclude ".gitmodules" --exclude "install.sh" -av . ~
+rsync --exclude ".git" \
+      --exclude ".gitmodules" \
+      --exclude "install.sh" \
+      --exclude "README.md" \
+      -av . ~
